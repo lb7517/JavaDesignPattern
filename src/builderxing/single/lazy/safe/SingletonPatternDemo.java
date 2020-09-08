719b63752d9c8f238b6545c5bc5e4fa23e6c3b18
lb7517 <lb_987@163.com>
2020-09-08 11:45:48 +0800

    添加多线程测试单例实现是否是线程安全

--- src/builderxing/single/lazy/safe/SingletonPatternDemo.java
+++ src/builderxing/single/lazy/safe/SingletonPatternDemo.java
@@ -1,8 +1,46 @@
 package builderxing.single.lazy.safe;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class SingletonPatternDemo {
-    public static void main(String args[]){
-        Singleton singleton = Singleton.getInstance();
-        singleton.getMessage();
+
+    private static AtomicInteger num = new AtomicInteger();
+
+    // 请求总数
+    public static int clientTotals = 200;
+
+    public static void main(String args[]) throws InterruptedException {
+        // 普通执行
+//        Singleton.getInstance().getMessage();
+
+        // 多线程环境
+        testUnsafe();
+
+    }
+
+    /**
+     *  通过线程池测试单例是否安全
+     * */
+    public static void testUnsafe() throws InterruptedException {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(8,
+                16, 60, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(100000));
+        CountDownLatch cd = new CountDownLatch(clientTotals);
+        for(int i =0; i < clientTotals; i++){
+            threadPoolExecutor.execute(() -> {
+               Singleton singleton = Singleton.getInstance();
+                System.out.println("ThreadName: "+Thread.currentThread().getName()
+                        + ", singleton: "+singleton);
+                singleton.getMessage();
+                System.out.println(num.incrementAndGet());
+                cd.countDown();
+            });
+        }
+        cd.await();
+        System.out.println("主线程执行完毕");
     }
 }
--- src/builderxing/single/unlazy/safe/SingletonPatternDemo.java
+++ src/builderxing/single/unlazy/safe/SingletonPatternDemo.java
@@ -1,8 +1,41 @@
 package builderxing.single.unlazy.safe;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class SingletonPatternDemo {
-    public static void main(String args[]){
-        Singleton singleton = Singleton.getInstance();
-        singleton.getMessage();
+
+    private static Integer clientTotals = 200;
+    private static AtomicInteger num = new AtomicInteger();
+
+    public static void main(String args[]) throws InterruptedException {
+        // 单例测试
+//        Singleton.instance.getMessage();
+
+        // 多线程测试单例
+        testSafe();
+    }
+
+    /**
+     * 多线程测试单例
+     * */
+    public static void testSafe() throws InterruptedException {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(8, 16, 60, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(100000));
+        CountDownLatch cd = new CountDownLatch(clientTotals);
+        for(int i =0; i < clientTotals; i++){
+            threadPoolExecutor.execute(() -> {
+                Singleton singleton = Singleton.getInstance();
+                System.out.println("ThreadName: "+Thread.currentThread().getName()
+                        + ", singleton: "+singleton);
+                singleton.getMessage();
+                cd.countDown();
+            });
+        }
+        cd.await();
+        System.out.println("主线程执行完毕");
     }
 }
